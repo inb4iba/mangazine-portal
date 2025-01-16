e0dd3e16fc7f7db70b6275a4e70c1960f15ebbc7
Icaro Andrade <iba1987@gmail.com>
2025-01-16 12:44:35 -0300

    add sanity schemas

---
+++ src/sanity/schemaTypes/authorType.ts
@@ -0,0 +1,28 @@
+import { title } from "process";
+import { defineField, defineType } from "sanity";
+
+export const authorType = defineType({
+    name: "author",
+    title: "Autor",
+    type: "document",
+    fields: [
+        defineField({
+            name: "name",
+            title: "Nome",
+            type: "string",
+            validation: Rule => Rule.required()
+        }),
+        defineField({
+            name: "avatar",
+            title: "Avatar",
+            type: "image",
+            validation: Rule => Rule.required()
+        })
+    ],
+    preview: {
+        select: {
+            title: "name",
+            media: "avatar"
+        }
+    }
+})
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { authorType } from './authorType'
+import { postType } from './postType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [authorType, postType],
 }
