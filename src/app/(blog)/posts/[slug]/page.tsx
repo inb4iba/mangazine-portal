07a9cd35b328d5aaceaea75fd8341eed42f6d64b
Icaro Andrade <iba1987@gmail.com>
2025-01-17 17:27:31 -0300

    update post page layout

--- src/app/(blog)/posts/[slug]/page.tsx
+++ src/app/(blog)/posts/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { AuthorNameplate } from "@/components/AuthorNameplate";
+import { PostCoverImage } from "@/components/PostCoverImage";
 import { urlFor } from "@/sanity/lib/image";
 import { sanityFetch } from "@/sanity/lib/live";
 import { SINGLE_POST_QUERY } from "@/sanity/queries/posts";
@@ -26,27 +27,78 @@ const Post = async ({ params }: PostProps) => {
 
   return (
     <main className="flex flex-1 justify-center">
-      <section className="flex flex-col xl:w-[1248px] my-8 gap-5">
-        <div className="relative h-60">
-          <Image
-            src={urlFor(post.cover).url()}
-            alt="cover image"
-            fill
-            className="object-cover"
-          />
+      <div className="flex flex-col xl:w-[1248px] mt-2 mb-8 gap-8">
+        <PostCoverImage
+          coverUrl={urlFor(post.cover).url()}
+          title={post.title}
+        />
+        <div className="flex flex-grow gap-20 relative">
+          <section className="flex-grow px-6 py-4">
+            <article className="prose prose-lg">
+              <PortableText value={post.body} />
+            </article>
+          </section>
+          <section className="flex flex-col w-96 sticky top-0 h-fit">
+            <div className="flex w-full py-4 px-6">
+              <ul className="flex flex-col gap-2 font-semibold text-lg">
+                <li>
+                  <a href="#1" className="hover:text-violet-600">
+                    Intro
+                  </a>
+                </li>
+                <li>
+                  <a href="#2" className="hover:text-violet-600">
+                    Capítulo 1
+                  </a>
+                </li>
+                <li>
+                  <a href="#3" className="hover:text-violet-600">
+                    Capítulo 2
+                  </a>
+                </li>
+                <li>
+                  <a href="#4" className="hover:text-violet-600">
+                    Conclusão
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="mx-auto w-11/12 h-[1px] bg-zinc-200"></div>
+            <div className="flex flex-col w-full py-4 px-6 gap-2">
+              <span className="font-medium">Escrito por</span>
+              <AuthorNameplate author={post.author} />
+            </div>
+            <div className="mx-auto w-11/12 h-[1px] bg-zinc-200"></div>
+            <div className="flex flex-col w-full py-4 px-6 gap-2">
+              <p className="font-medium">
+                Data de publicação:{" "}
+                <span className="text-violet-600">
+                  {formatDate(post.created_at)}
+                </span>
+              </p>
+            </div>
+          </section>
         </div>
-        <h1 className="text-4xl font-bold text-zinc-800">{post.title}</h1>
-        <div className="flex gap-4">
-          <AuthorNameplate author={post.author} />
-          <span>{formatDate(post.created_at)}</span>
-        </div>
-        <article className="flex flex-col prose prose-lg max-w-none w-8/12">
-          <PortableText value={post.body} />
-        </article>
-
-        {post.podcast && <span>Tem podcast</span>}
-      </section>
+      </div>
     </main>
+    // <main className="flex flex-1 justify-center">
+    //   <div className="flex flex-col xl:w-[1248px] my-8 gap-5">
+    //     <div className="relative h-60">
+    //       <Image
+    //         src={urlFor(post.cover).url()}
+    //         alt="cover image"
+    //         fill
+    //         className="object-cover fixed"
+    //       />
+    //     </div>
+    //     <h1 className="text-4xl font-bold text-zinc-800">{post.title}</h1>
+    //     <div className="flex gap-4">
+    //       <AuthorNameplate author={post.author} />
+    //       <span>{formatDate(post.created_at)}</span>
+    //     </div>
+    //     {post.podcast && <span>Tem podcast</span>}
+    //   </div>
+    // </main>
   );
 };
 
---
+++ src/components/PostCoverImage.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+type Props = {
+  coverUrl: string;
+  title: string;
+};
+
+export const PostCoverImage = ({ coverUrl, title }: Props) => {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setOffset(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <section className="bg-red-500 w-full h-96 rounded-b-3xl relative overflow-hidden">
+      <Image
+        src={coverUrl}
+        alt="cover image"
+        fill
+        className="object-cover"
+        style={{
+          objectPosition: `center ${25 + offset * 0.08}%`,
+          transition: "object-position 0.05s linear",
+        }}
+      />
+
+      <div className="bg-gradient-to-b from-transparent to-zinc-900 absolute w-full h-full z-50 flex flex-col justify-end px-8 py-4 gap-2">
+        <h1 className="text-5xl text-zinc-100 font-bold w-10/12">{title}</h1>
+        <p className="text-zinc-100 text-xl w-3/4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit assumenda
+          fugit hic ullam libero sunt consectetur qui consequatur ea.
+        </p>
+      </div>
+    </section>
+  );
+};
