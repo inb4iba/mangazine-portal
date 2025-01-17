049696ac0fd92e7603a5f21bbf1b5100c4c9abed
Icaro Andrade <iba1987@gmail.com>
2025-01-17 00:26:10 -0300

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="flex justify-center text-zinc-800 border-t">
+      <div className="flex justify-center xl:w-[1248px] py-6 items-center">
+        <span className="font-medium italic text-lg">
+          Feito de otakus para otakus.
+        </span>
+      </div>
+    </footer>
+  );
+};
