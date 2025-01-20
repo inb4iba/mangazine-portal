8afd0c0671b362d65f45750fcb0ec0ef30366d15
Icaro Andrade <iba1987@gmail.com>
2025-01-20 00:09:06 -0300

    add bg to header on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="flex justify-center text-zinc-800 top-0 sticky sm:relative z-50">
+    <header className="flex justify-center text-zinc-800 top-0 sticky sm:relative z-50 bg-white/65 backdrop-blur-md sm:bg-transparent">
       <div className="flex flex-grow xl:flex-grow-0 justify-between xl:w-[1248px] p-4 items-center sm:p-8">
         <Link href="/" className={`${roboto.className} text-3xl font-black`}>
           mangazine
