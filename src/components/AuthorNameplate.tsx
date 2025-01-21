d23a857578d74002637a4a5ad137b8571fe17ff8
Icaro Andrade <iba1987@gmail.com>
2025-01-20 23:58:55 -0300

    add href to social links

--- src/components/AuthorNameplate.tsx
+++ src/components/AuthorNameplate.tsx
@@ -11,6 +11,7 @@ import myanimelist from "@/socials/myanimelist.svg";
 import youtube from "@/socials/youtube.svg";
 import tiktok from "@/socials/TikTok.svg";
 import spotify from "@/socials/spotify.svg";
+import Link from "next/link";
 
 type AuthorNameplateProps = {
   author: NonNullable<SINGLE_POST_QUERYResult>["author"];
@@ -35,79 +36,112 @@ export const AuthorNameplate = ({
         />
         <span className="font-medium">{author.name}</span>
       </div>
+      {/* TODO - Move socials to another component and rethink the logic to automate socials */}
       {showSocials && (
         <div className="flex gap-2">
           {author.socials?.instagram && (
-            <Image
-              src={instagram}
-              alt="Instagram icon"
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
+            <Link
+              href={`https://instagram.com/${author.socials.instagram}`}
+              target="_blank">
+              <Image
+                src={instagram}
+                alt="Instagram icon"
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+            </Link>
           )}
           {author.socials?.twitter && (
-            <Image
-              src={twitter}
-              alt="X icon"
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
+            <Link
+              href={`https://x.com/${author.socials.twitter}`}
+              target="_blank">
+              <Image
+                src={twitter}
+                alt="X icon"
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+            </Link>
           )}
           {author.socials?.bluesky && (
-            <Image
-              src={bluesky}
-              alt="BlueSky icon"
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
+            <Link
+              href={`https://bsky.app/${author.socials.bluesky}`}
+              target="_blank">
+              <Image
+                src={bluesky}
+                alt="BlueSky icon"
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+            </Link>
           )}
           {author.socials?.anilist && (
-            <Image
-              src={anilist}
-              alt="AniList icon"
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
+            <Link
+              href={`https://anilist.co/user/${author.socials.anilist}`}
+              target="_blank">
+              <Image
+                src={anilist}
+                alt="AniList icon"
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+            </Link>
           )}
           {author.socials?.myanimelist && (
-            <Image
-              src={myanimelist}
-              alt="My Anime List icon"
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
+            <Link
+              href={`https://myanimelist.net/profile/${author.socials.myanimelist}`}
+              target="_blank">
+              <Image
+                src={myanimelist}
+                alt="My Anime List icon"
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+            </Link>
           )}
           {author.socials?.youtube && (
-            <Image
-              src={youtube}
-              alt="YouTube icon"
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
+            <Link
+              href={`https://youtube.com/${author.socials.youtube}`}
+              target="_blank">
+              <Image
+                src={youtube}
+                alt="YouTube icon"
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+            </Link>
           )}
           {author.socials?.tiktok && (
-            <Image
-              src={tiktok}
-              alt="TikTok icon"
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
+            <Link
+              href={`https://tiktok.com/@${author.socials.tiktok}`}
+              target="_blank">
+              <Image
+                src={tiktok}
+                alt="TikTok icon"
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+            </Link>
           )}
           {author.socials?.spotify && (
-            <Image
-              src={spotify}
-              alt="Spotify icon"
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
+            <Link
+              href={`https://spotify.com/user/${author.socials.spotify}`}
+              target="_blank">
+              <Image
+                src={spotify}
+                alt="Spotify icon"
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+            </Link>
           )}
         </div>
       )}
