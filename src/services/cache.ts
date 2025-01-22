3b07acf4a60e1b5d2bf21a9619c440911d875b27
Icaro Andrade <iba1987@gmail.com>
2025-01-22 16:22:10 -0300

    add cache in memory

---
+++ src/services/cache.ts
@@ -0,0 +1,37 @@
+export type OfferDetail = {
+  title: string;
+  img: string;
+  price: string;
+  fullPrice: string;
+};
+
+type CacheValue = {
+  value: OfferDetail;
+  expiration: number;
+};
+
+const cache: Record<string, CacheValue> = {};
+
+export const setCache = (key: string, value: OfferDetail) => {
+  cache[key] = {
+    value,
+    expiration: Date.now() + 1000 * 60 * 60 * 24,
+  };
+};
+
+export const getCache = (key: string) => {
+  const cachedItem = cache[key];
+  if (!cachedItem) return null;
+  return cachedItem.value;
+};
+
+export const isCacheValid = (key: string) => {
+  const cachedItem = cache[key];
+  if (!cachedItem) return false;
+  if (Date.now() >= cachedItem.expiration) {
+    console.log(`Deletando cache expirado: ${key}`);
+    delete cache[key];
+    return false;
+  }
+  return true;
+};
